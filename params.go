415ecbe9cbc9a73b6b67fac3c0b83a07a1c5b65a
FloodCode <flood.code@gmail.com>
2017-08-31 00:17:11 +0300

    Add ability to send files over API Implement new API methods (getMe, forwardMessage, sendPhoto) Improve request handling

--- params.go
+++ params.go
@@ -3,7 +3,10 @@ package tgbot
 import (
   "strconv"
   "strings"
+  "io/ioutil"
 )
+
+// API method option ChatIdentifier
 
 type ChatIdentifier struct {
   chatId string
@@ -25,6 +28,8 @@ func Username(username string) *ChatIdentifier {
   }
 }
 
+// API method option ParseMode
+
 type ParseMode struct {
   parseMode string
 }
@@ -41,9 +46,37 @@ func ParseModeHTML() *ParseMode {
   return &ParseMode{parseMode: "HTML"}
 }
 
+// API method option ReplyMarkup
+// TODO: Implement logic
+
 type ReplyMarkup struct {
   markup string
 }
+
+// API method option InputFile
+
+type InputFile struct {
+  fileData interface{}
+}
+
+func (f *InputFile) Get() interface{} {
+  return f.fileData
+}
+
+func FileId(fileId string) *InputFile {
+  return &InputFile{fileData: fileId}
+}
+
+func FileBytes(fileData []byte) *InputFile {
+  return &InputFile{fileData: fileData}
+}
+
+func FilePath(filePath string) *InputFile {
+  bytes, _ := ioutil.ReadFile(filePath)
+  return &InputFile{fileData: bytes}
+}
+
+// API method parameters
 
 type ParamsGetUpdates struct {
   Offset  int `option:"offset"`
@@ -56,6 +89,22 @@ type ParamsSendMessage struct {
   Text                  string          `option:"text"                      required:"true"`
   ParseMode             *ParseMode      `option:"parse_mode"`
   DisableWebPagePreview bool            `option:"disable_web_page_preview"`
+  DisableNotification   bool            `option:"disable_notification"`
+  ReplyToMessageId      int             `option:"reply_to_message_id"`
+  ReplyMarkup           *ReplyMarkup    `option:"reply_markup"`
+}
+
+type ParamsForwardMessage struct {
+  ChatId                *ChatIdentifier `option:"chat_id"                   required:"true"`
+  FromChatId            *ChatIdentifier `option:"from_chat_id"              required:"true"`
+  DisableNotification   bool            `option:"disable_notification"`
+  MessageId             int             `option:"message_id"                required:"true"`
+}
+
+type ParamsSendPhoto struct {
+  ChatId                *ChatIdentifier `option:"chat_id" required:"true"`
+  Photo                 *InputFile      `option:"photo"   required:"true"`
+  Caption               string          `option:"caption"`
   DisableNotification   bool            `option:"disable_notification"`
   ReplyToMessageId      int             `option:"reply_to_message_id"`
   ReplyMarkup           *ReplyMarkup    `option:"reply_markup"`
