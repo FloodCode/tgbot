df20abded38db5c9cf2fc3fd904906ec39dc7ab0
FloodCode <flood.code@gmail.com>
2017-09-04 22:44:06 +0300

    Add new types and methods Reduse method declaration size

--- tgbot.go
+++ tgbot.go
@@ -38,9 +38,8 @@ func (b *TelegramBot) Poll() {
 	for true {
 		var updates, err = b.GetUpdates(ParamsGetUpdates{Offset: b.lastUpdateID + 1})
 		if err == nil && len(updates) != 0 {
-			var lastUpdate = updates[len(updates)-1]
-			b.updatesCallback(updates)
-			b.lastUpdateID = lastUpdate.UpdateID
+			b.lastUpdateID = updates[len(updates)-1].UpdateID
+			go b.updatesCallback(updates)
 		}
 
 		time.Sleep(time.Duration(b.poolDelay) * time.Millisecond)
@@ -49,86 +48,92 @@ func (b *TelegramBot) Poll() {
 
 // GetMe returns basic information about the bot.
 func (b TelegramBot) GetMe() (me User, err error) {
-	err = b.sendResuest("getme", nil, &me)
-	return me, err
+	return me, b.sendResuest("getme", nil, &me)
 }
 
 // GetUpdates allows to get new updates.
 func (b TelegramBot) GetUpdates(params ParamsGetUpdates) (updates []Update, err error) {
-	err = b.sendResuest("getUpdates", params, &updates)
-	return updates, err
+	return updates, b.sendResuest("getUpdates", params, &updates)
 }
 
 // SendMessage sends text message.
 func (b TelegramBot) SendMessage(params ParamsSendMessage) (message Message, err error) {
-	err = b.sendResuest("sendMessage", params, &message)
-	return message, err
+	return message, b.sendResuest("sendMessage", params, &message)
 }
 
 // ForwardMessage re-sends message of any type.
 func (b TelegramBot) ForwardMessage(params ParamsForwardMessage) (message Message, err error) {
-	err = b.sendResuest("forwardMessage", params, &message)
-	return message, err
+	return message, b.sendResuest("forwardMessage", params, &message)
 }
 
 // SendPhoto sends photo message.
 func (b TelegramBot) SendPhoto(params ParamsSendPhoto) (message Message, err error) {
-	err = b.sendResuest("sendPhoto", params, &message)
-	return message, err
+	return message, b.sendResuest("sendPhoto", params, &message)
 }
 
 // SendAudio sends audio message.
 func (b TelegramBot) SendAudio(params ParamsSendAudio) (message Message, err error) {
-	err = b.sendResuest("sendAudio", params, &message)
-	return message, err
+	return message, b.sendResuest("sendAudio", params, &message)
 }
 
 // SendDocument sends document message.
 func (b TelegramBot) SendDocument(params ParamsSendDocument) (message Message, err error) {
-	err = b.sendResuest("sendDocument", params, &message)
-	return message, err
+	return message, b.sendResuest("sendDocument", params, &message)
 }
 
 // SendVideo sends video message.
 func (b TelegramBot) SendVideo(params ParamsSendVideo) (message Message, err error) {
-	err = b.sendResuest("sendVideo", params, &message)
-	return message, err
+	return message, b.sendResuest("sendVideo", params, &message)
 }
 
 // SendVoice sends voice note message.
 func (b TelegramBot) SendVoice(params ParamsSendVoice) (message Message, err error) {
-	err = b.sendResuest("sendVoice", params, &message)
-	return message, err
+	return message, b.sendResuest("sendVoice", params, &message)
 }
 
 // SendVideoNote sends video note message.
 func (b TelegramBot) SendVideoNote(params ParamsSendVideoNote) (message Message, err error) {
-	err = b.sendResuest("sendVideoNote", params, &message)
-	return message, err
+	return message, b.sendResuest("sendVideoNote", params, &message)
 }
 
 // SendLocation sends location message.
 func (b TelegramBot) SendLocation(params ParamsSendLocation) (message Message, err error) {
-	err = b.sendResuest("sendLocation", params, &message)
-	return message, err
+	return message, b.sendResuest("sendLocation", params, &message)
 }
 
 // SendVenue sends information about a venue.
 func (b TelegramBot) SendVenue(params ParamsSendVenue) (message Message, err error) {
-	err = b.sendResuest("sendVenue", params, &message)
-	return message, err
+	return message, b.sendResuest("sendVenue", params, &message)
 }
 
 // SendContact sends phone contact.
 func (b TelegramBot) SendContact(params ParamsSendContact) (message Message, err error) {
-	err = b.sendResuest("sendContact", params, &message)
-	return message, err
+	return message, b.sendResuest("sendContact", params, &message)
 }
 
 // SendChatAction sends phone contact.
 func (b TelegramBot) SendChatAction(params ParamsSendChatAction) (success bool, err error) {
-	err = b.sendResuest("sendChatAction", params, &success)
-	return success, err
+	return success, b.sendResuest("sendChatAction", params, &success)
+}
+
+// GetUserProfilePhotos returns user profile photos.
+func (b TelegramBot) GetUserProfilePhotos(params ParamsGetUserProfilePhotos) (photos UserProfilePhotos, err error) {
+	return photos, b.sendResuest("getUserProfilePhotos", params, &photos)
+}
+
+// GetFile allows to get basic info about a file and prepare it for downloading.
+func (b TelegramBot) GetFile(params ParamsGetFile) (file File, err error) {
+	return file, b.sendResuest("getFile", params, &file)
+}
+
+// KickChatMember allows to kick user from a group, a supergroup or a channel.
+func (b TelegramBot) KickChatMember(params ParamsKickChatMember) (success bool, err error) {
+	return success, b.sendResuest("kickChatMember", params, &success)
+}
+
+// UnbanChatMember allows to unban user from a group, a supergroup or a channel.
+func (b TelegramBot) UnbanChatMember(params ParamsUnbanChatMember) (success bool, err error) {
+	return success, b.sendResuest("unbanChatMember", params, &success)
 }
 
 func (b TelegramBot) sendResuest(method string, paramsObject interface{}, t interface{}) error {
