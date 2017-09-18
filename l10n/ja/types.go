1614f561fd29cd8a84d769b92033be5120701586
Meow <flood.code@gmail.com>
2017-09-18 15:45:08 +0300

    New translations types.go (Japanese)

---
+++ l10n/ja/types.go
@@ -0,0 +1,378 @@
+package tgbot
+
+import "encoding/json"
+
+// Update represents an incoming update.
+type Update struct {
+	UpdateID           int                 `json:"update_id"`
+	Message            *Message            `json:"message"`
+	EditedMessage      *Message            `json:"edited_message"`
+	ChannelPost        *Message            `json:"channel_post"`
+	EditedChannelPost  *Message            `json:"edited_channel_post"`
+	InlineQuery        *InlineQuery        `json:"inline_query"`
+	ChosenInlineResult *ChosenInlineResult `json:"chosen_inline_result"`
+	CallbackQuery      *CallbackQuery      `json:"callback_query"`
+	ShippingQuery      *ShippingQuery      `json:"shipping_query"`
+	PreCheckoutQuery   *PreCheckoutQuery   `json:"pre_checkout_query"`
+}
+
+// Message represents a message.
+type Message struct {
+	MessageID             int                `json:"message_id"`
+	From                  *User              `json:"from"`
+	Date                  int                `json:"date"`
+	Chat                  *Chat              `json:"chat"`
+	ForwardFrom           *User              `json:"forward_from"`
+	ForwardFromChat       *Chat              `json:"forward_from_chat"`
+	ForwardFromMessageID  int                `json:"forward_from_message_id"`
+	ForwardSignature      string             `json:"forward_signature"`
+	ForwardDate           int                `json:"forward_date"`
+	ReplyToMessage        *Message           `json:"reply_to_message"`
+	EditDate              int                `json:"edit_date"`
+	AuthorSignature       string             `json:"author_signature"`
+	Text                  string             `json:"text"`
+	Entities              *[]MessageEntity   `json:"entities"`
+	Audio                 *Audio             `json:"audio"`
+	Document              *Document          `json:"document"`
+	Game                  *Game              `json:"game"`
+	Photo                 *[]PhotoSize       `json:"photo"`
+	Sticker               *Sticker           `json:"sticker"`
+	Video                 *Video             `json:"video"`
+	Voice                 *Voice             `json:"voice"`
+	VideoNote             *VideoNote         `json:"video_note"`
+	NewChatMembers        *[]User            `json:"new_chat_members"`
+	Caption               string             `json:"caption"`
+	Contact               *Contact           `json:"contact"`
+	Location              *Location          `json:"location"`
+	Venue                 *Venue             `json:"venue"`
+	NewChatMember         *User              `json:"new_chat_member"`
+	LeftChatMember        *User              `json:"left_chat_member"`
+	NewChatTitle          string             `json:"new_chat_title"`
+	NewChatPhoto          *[]PhotoSize       `json:"new_chat_photo"`
+	DeleteChatPhoto       bool               `json:"delete_chat_photo"`
+	GroupChatCreated      bool               `json:"group_chat_created"`
+	SupergroupChatCreated bool               `json:"supergroup_chat_created"`
+	ChannelChatCreated    bool               `json:"channel_chat_created"`
+	MigrateToChatID       int64              `json:"migrate_to_chat_id"`
+	MigrateFromChatID     int64              `json:"migrate_from_chat_id"`
+	PinnedMessage         *Message           `json:"pinned_message"`
+	SuccessfulPayment     *SuccessfulPayment `json:"successful_payment"`
+}
+
+// User represents a user.
+type User struct {
+	ID           int    `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Username     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+
+// Chat represents a chat.
+type Chat struct {
+	ID                          int64      `json:"id"`
+	Type                        string     `json:"type"`
+	Title                       string     `json:"title"`
+	Username                    string     `json:"username"`
+	FirstName                   string     `json:"first_name"`
+	LastName                    string     `json:"last_name"`
+	AllMembersAreAdministrators bool       `json:"all_members_are_administrators"`
+	Photo                       *ChatPhoto `json:"photo"`
+	Description                 string     `json:"description"`
+	InviteLink                  string     `json:"invite_link"`
+	PinnedMessage               *Message   `json:"pinned_message"`
+}
+
+// ChatPhoto represents a chat photo.
+type ChatPhoto struct {
+	SmallFileID string `json:"small_file_id"`
+	BigFileID   string `json:"big_file_id"`
+}
+
+// MessageEntity represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
+type MessageEntity struct {
+	Type   string `json:"type"`
+	Offset int    `json:"offset"`
+	Length int    `json:"length"`
+	URL    string `json:"url"`
+	User   *User  `json:"user"`
+}
+
+// Audio represents an audio file to be treated as music by the Telegram clients.
+type Audio struct {
+	FileID    string `json:"file_id"`
+	Duration  int    `json:"duration"`
+	Performer string `json:"performer"`
+	Title     string `json:"title"`
+	MimeType  string `json:"mime_type"`
+	FileSize  int    `json:"file_size"`
+}
+
+// Document represents a general file (as opposed to photos, voice messages and audio files).
+type Document struct {
+	FileID   string     `json:"file_id"`
+	Thumb    *PhotoSize `json:"thumb"`
+	FileName string     `json:"file_name"`
+	MimeType string     `json:"mime_type"`
+	FileSize int        `json:"file_size"`
+}
+
+// PhotoSize represents one size of a photo or a file / sticker thumbnail.
+type PhotoSize struct {
+	FileID   string `json:"file_id"`
+	Width    int    `json:"width"`
+	Height   int    `json:"height"`
+	FileSize int    `json:"file_size"`
+}
+
+// Game represents a game.
+// Use BotFather to create and edit games, their short names will act as unique identifiers.
+type Game struct {
+	Title        string           `json:"title"`
+	Description  string           `json:"description"`
+	Photo        *PhotoSize       `json:"photo"`
+	Text         string           `json:"text"`
+	TextEntities *[]MessageEntity `json:"text_entities"`
+	Animation    *Animation       `json:"animation"`
+}
+
+// Animation represents an animation file to be displayed in the message containing a game.
+type Animation struct {
+	FileID   string     `json:"file_id"`
+	Thumb    *PhotoSize `json:"thumb"`
+	FileName string     `json:"file_name"`
+	MimeType string     `json:"mime_type"`
+	FileSize int        `json:"file_size"`
+}
+
+// Sticker represents a sticker.
+type Sticker struct {
+	FileID       string        `json:"file_id"`
+	Width        int           `json:"width"`
+	Height       int           `json:"height"`
+	Thumb        *PhotoSize    `json:"thumb"`
+	Emoji        string        `json:"emoji"`
+	SetName      string        `json:"set_name"`
+	MaskPosition *MaskPosition `json:"mask_position"`
+	FileSize     int           `json:"file_size"`
+}
+
+// StickerSet represents a sticker set.
+type StickerSet struct {
+	Name          string     `json:"name"`
+	Title         string     `json:"title"`
+	ContainsMasks bool       `json:"contains_masks"`
+	Stickers      *[]Sticker `json:"stickers"`
+}
+
+// MaskPosition describes the position on faces where a mask should be placed by default.
+// TODO: Turn it into a method.
+type MaskPosition struct {
+	Point  string  `json:"point"`
+	XShift float32 `json:"x_shift"`
+	YShift float32 `json:"y_shift"`
+	Scale  float32 `json:"scale"`
+}
+
+// Video represents a video file.
+type Video struct {
+	FileID   string     `json:"file_id"`
+	Width    int        `json:"width"`
+	Height   int        `json:"height"`
+	Duration int        `json:"duration"`
+	Thumb    *PhotoSize `json:"thumb"`
+	MimeType string     `json:"mime_type"`
+	FileSize int        `json:"file_size"`
+}
+
+// Voice represents a voice note.
+type Voice struct {
+	FileID   string `json:"file_id"`
+	Duration int    `json:"duration"`
+	MimeType string `json:"mime_type"`
+	FileSize int    `json:"file_size"`
+}
+
+// VideoNote represents a video message.
+type VideoNote struct {
+	FileID   string     `json:"file_id"`
+	Length   int        `json:"length"`
+	Duration int        `json:"duration"`
+	Thumb    *PhotoSize `json:"thumb"`
+	FileSize int        `json:"file_size"`
+}
+
+// Contact represents a phone contact.
+type Contact struct {
+	PhoneNumber string `json:"phone_number"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	UserID      int    `json:"user_id"`
+}
+
+// Location represents a point on the map.
+type Location struct {
+	Longitude float32 `json:"longitude"`
+	Latitude  float32 `json:"latitude"`
+}
+
+// Venue represents a venue.
+type Venue struct {
+	Location     *Location `json:"location"`
+	Title        string    `json:"title"`
+	Address      string    `json:"address"`
+	FoursquareID string    `json:"foursquare_id"`
+}
+
+// Invoice contains basic information about a invoice.
+type Invoice struct {
+	Title          string `json:"title"`
+	Description    string `json:"description"`
+	StartParameter string `json:"start_parameter"`
+	Currency       string `json:"currency"`
+	TotalAmount    int    `json:"total_amount"`
+}
+
+// SuccessfulPayment contains basic information about a successful payment.
+type SuccessfulPayment struct {
+	Currency                string     `json:"currency"`
+	TotalAmount             int        `json:"total_amount"`
+	InvoicePayload          string     `json:"invoice_payload"`
+	ShippingOptionID        string     `json:"shipping_option_id"`
+	OrderInfo               *OrderInfo `json:"order_info"`
+	TelegramPaymentChargeID string     `json:"telegram_payment_charge_id"`
+	ProviderPaymentChargeID string     `json:"provider_payment_charge_id"`
+}
+
+// OrderInfo represents information about an order.
+type OrderInfo struct {
+	Name            string           `json:"name"`
+	PhoneNumber     string           `json:"phone_number"`
+	Email           string           `json:"email"`
+	ShippingAddress *ShippingAddress `json:"shipping_address"`
+}
+
+// ShippingAddress represents a shipping address.
+type ShippingAddress struct {
+	CountryCode string `json:"country_code"`
+	State       string `json:"state"`
+	City        string `json:"city"`
+	StreetLine1 string `json:"street_line1"`
+	StreetLine2 string `json:"street_line2"`
+	PostCode    string `json:"post_code"`
+}
+
+// UserProfilePhotos represents a user's profile pictures.
+type UserProfilePhotos struct {
+	TotalCount int            `json:"total_count"`
+	Photos     *[][]PhotoSize `json:"photos"`
+}
+
+// File represents a file ready to be downloaded.
+type File struct {
+	FileID   string `json:"file_id"`
+	FileSize int    `json:"file_size"`
+	FilePath string `json:"file_path"`
+}
+
+// ChatMember represents information about one member of a chat.
+type ChatMember struct {
+	User                  *User  `json:"user"`
+	Status                string `json:"status"`
+	UntilDate             int    `json:"until_date"`
+	CanBeEdited           bool   `json:"can_be_edited"`
+	CanChangeInfo         bool   `json:"can_change_info"`
+	CanPostMessages       bool   `json:"can_post_messages"`
+	CanEditMessages       bool   `json:"can_edit_messages"`
+	CanDeleteMessages     bool   `json:"can_delete_messages"`
+	CanInviteUsers        bool   `json:"can_invite_users"`
+	CanRestrictMembers    bool   `json:"can_restrict_members"`
+	CanPinMessages        bool   `json:"can_pin_messages"`
+	CanPromoteMembers     bool   `json:"can_promote_members"`
+	CanSendMessages       bool   `json:"can_send_messages"`
+	CanSendMediaMessages  bool   `json:"can_send_media_messages"`
+	CanSendOtherMessages  bool   `json:"can_send_other_messages"`
+	CanAddWebPagePreviews bool   `json:"can_add_web_page_previews"`
+}
+
+// CallbackGame is a placeholder, currently holds no information.
+// Use BotFather to set up your game.
+type CallbackGame struct{}
+
+// InlineKeyboardButton represents one button of an inline keyboard.
+// You must use exactly one of the optional fields.
+type InlineKeyboardButton struct {
+	Text                         string        `json:"text"`
+	URL                          string        `json:"url"`
+	CallbackData                 string        `json:"callback_data"`
+	SwitchInlineQuery            string        `json:"switch_inline_query"`
+	SwitchInlineQueryCurrentChat string        `json:"switch_inline_query_current_chat"`
+	CallbackGame                 *CallbackGame `json:"callback_game"`
+	Pay                          bool          `json:"pay"`
+}
+
+// KeyboardButton represents one button of the reply keyboard.
+type KeyboardButton struct {
+	Text            string `json:"text"`
+	RequestContact  bool   `json:"request_contact"`
+	RequestLocation bool   `json:"request_location"`
+}
+
+// InlineQuery represents an incoming inline query.
+// When the user sends an empty query, your bot could return some default or trending results.
+type InlineQuery struct {
+	ID       string    `json:"id"`
+	From     *User     `json:"from"`
+	Location *Location `json:"location"`
+	Query    string    `json:"query"`
+	Offset   string    `json:"offset"`
+}
+
+// ChosenInlineResult represents a result of an inline query
+// that was chosen by the user and sent to their chat partner.
+type ChosenInlineResult struct {
+	ResultID        string    `json:"result_id"`
+	From            *User     `json:"from"`
+	Location        *Location `json:"location"`
+	InlineMessageID string    `json:"inline_message_id"`
+	Query           string    `json:"query"`
+}
+
+// CallbackQuery represents an incoming callback query from a callback button
+// in an inline keyboard. If the button that originated the query was attached
+// to a message sent by the bot, the field message will be present.
+type CallbackQuery struct {
+	ID              string   `json:"id"`
+	From            *User    `json:"from"`
+	Message         *Message `json:"message"`
+	InlineMessageID string   `json:"inline_message_id"`
+	ChatInstance    string   `json:"chat_instance"`
+	Data            string   `json:"data"`
+	GameShortName   string   `json:"game_short_name"`
+}
+
+// ShippingQuery represents a shipping address.
+type ShippingQuery struct {
+	ID              string           `json:"id"`
+	From            *User            `json:"from"`
+	InvoicePayload  string           `json:"invoice_payload"`
+	ShippingAddress *ShippingAddress `json:"shipping_address"`
+}
+
+// PreCheckoutQuery represents information about an incoming pre-checkout query.
+type PreCheckoutQuery struct {
+	ID               string     `json:"id"`
+	From             *User      `json:"from"`
+	Currency         string     `json:"currency"`
+	TotalAmount      int        `json:"total_amount"`
+	InvoicePayload   string     `json:"invoice_payload"`
+	ShippingOptionID string     `json:"shipping_option_id"`
+	OrderInfo        *OrderInfo `json:"order_info"`
+}
+
+type apiResponse struct {
+	Ok          bool            `json:"ok"`
+	Result      json.RawMessage `json:"result"`
+	Description string          `json:"description"`
+}
