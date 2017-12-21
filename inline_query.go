ec17bef4204d72c3b42a22de5e678bf35acc5c46
FloodCode <flood.code@gmail.com>
2017-12-21 23:26:23 +0200

    Add all non-cached inline query results

--- inline_query.go
+++ inline_query.go
@@ -23,9 +23,7 @@ func (r InlineQueryResultArticle) getType() string {
 	return "article"
 }
 
-// InlineQueryResultPhoto represents a link to a photo. By default, this photo will be sent by the user
-// with optional caption. Alternatively, you can use InputMessageContent to send a message with the specified content
-// instead of the photo
+// InlineQueryResultPhoto represents a link to a photo
 type InlineQueryResultPhoto struct {
 	ID                  string               `json:"id"`
 	PhotoURL            string               `json:"photo_url"`
@@ -41,6 +39,176 @@ type InlineQueryResultPhoto struct {
 
 func (r InlineQueryResultPhoto) getType() string {
 	return "photo"
+}
+
+// InlineQueryResultGif represents a link to an animated GIF file
+type InlineQueryResultGif struct {
+	ID                  string               `json:"id"`
+	GifURL              string               `json:"gif_url"`
+	GifWidth            int                  `json:"gif_width"`
+	GifHeight           int                  `json:"gif_height"`
+	GifDuration         int                  `json:"gif_duration"`
+	ThumbURL            string               `json:"thumb_url"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+}
+
+func (r InlineQueryResultGif) getType() string {
+	return "gif"
+}
+
+// InlineQueryResultMpeg4Gif represents a link to a video animation (H.264/MPEG-4 AVC video without sound)
+type InlineQueryResultMpeg4Gif struct {
+	ID                  string               `json:"id"`
+	Mpeg4URL            string               `json:"mpeg4_url"`
+	Mpeg4Width          int                  `json:"mpeg4_width"`
+	Mpeg4Height         int                  `json:"mpeg4_height"`
+	Mpeg4Duration       int                  `json:"mpeg4_duration"`
+	ThumbURL            string               `json:"thumb_url"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+}
+
+func (r InlineQueryResultMpeg4Gif) getType() string {
+	return "mpeg4_gif"
+}
+
+// InlineQueryResultVideo represents a link to a page containing an embedded video player or a video file
+type InlineQueryResultVideo struct {
+	ID                  string               `json:"id"`
+	VideoURL            string               `json:"video_url"`
+	MimeType            string               `json:"mime_type"`
+	ThumbURL            string               `json:"thumb_url"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	VideoWidth          int                  `json:"video_width"`
+	VideoHeight         int                  `json:"video_height"`
+	VideoDuration       int                  `json:"video_duration"`
+	Description         string               `json:"description"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+}
+
+func (r InlineQueryResultVideo) getType() string {
+	return "video"
+}
+
+// InlineQueryResultAudio represents a link to a voice recording in an .ogg container encoded with OPUS
+type InlineQueryResultAudio struct {
+	ID                  string               `json:"id"`
+	AudioURL            string               `json:"audio_url"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	Performer           string               `json:"performer"`
+	AudioDuration       int                  `json:"audio_duration"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+}
+
+func (r InlineQueryResultAudio) getType() string {
+	return "audio"
+}
+
+// InlineQueryResultVoice represents a link to a voice recording in an .ogg container encoded with OPUS
+type InlineQueryResultVoice struct {
+	ID                  string               `json:"id"`
+	VoiceURL            string               `json:"voice_url"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	VoiceDuration       int                  `json:"voice_duration"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+}
+
+func (r InlineQueryResultVoice) getType() string {
+	return "voice"
+}
+
+// InlineQueryResultDocument represents a link to a file
+type InlineQueryResultDocument struct {
+	ID                  string               `json:"id"`
+	Title               string               `json:"title"`
+	Caption             string               `json:"caption"`
+	DocumentURL         string               `json:"document_url"`
+	MimeType            string               `json:"mime_type"`
+	Description         string               `json:"description"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+	ThumbURL            string               `json:"thumb_url"`
+	ThumbWidth          int                  `json:"thumb_width"`
+	ThumbHeight         int                  `json:"thumb_height"`
+}
+
+func (r InlineQueryResultDocument) getType() string {
+	return "document"
+}
+
+// InlineQueryResultLocation represents a location on a map
+type InlineQueryResultLocation struct {
+	ID                  string               `json:"id"`
+	Latitude            float64              `json:"latitude"`
+	Longitude           float64              `json:"longitude"`
+	Title               string               `json:"title"`
+	LivePeriod          int                  `json:"live_period"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+	ThumbURL            string               `json:"thumb_url"`
+	ThumbWidth          int                  `json:"thumb_width"`
+	ThumbHeight         int                  `json:"thumb_height"`
+}
+
+func (r InlineQueryResultLocation) getType() string {
+	return "location"
+}
+
+// InlineQueryResultVenue represents a venue
+type InlineQueryResultVenue struct {
+	ID                  string               `json:"id"`
+	Latitude            float64              `json:"latitude"`
+	Longitude           float64              `json:"longitude"`
+	Title               string               `json:"title"`
+	Address             string               `json:"address"`
+	FoursquareID        string               `json:"foursquare_id"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+	ThumbURL            string               `json:"thumb_url"`
+	ThumbWidth          int                  `json:"thumb_width"`
+	ThumbHeight         int                  `json:"thumb_height"`
+}
+
+func (r InlineQueryResultVenue) getType() string {
+	return "venue"
+}
+
+// InlineQueryResultContact represents a contact with a phone number
+type InlineQueryResultContact struct {
+	ID                  string               `json:"id"`
+	PhoneNumber         string               `json:"phone_number"`
+	FirstName           string               `json:"first_name"`
+	LastName            string               `json:"last_name"`
+	ReplyMarkup         *ReplyMarkup         `json:"reply_markup"`
+	InputMessageContent *InputMessageContent `json:"input_message_content"`
+	ThumbURL            string               `json:"thumb_url"`
+	ThumbWidth          int                  `json:"thumb_width"`
+	ThumbHeight         int                  `json:"thumb_height"`
+}
+
+func (r InlineQueryResultContact) getType() string {
+	return "contact"
+}
+
+// InlineQueryResultGame represents a Game
+type InlineQueryResultGame struct {
+	ID            string `json:"id"`
+	GameShortName string `json:"game_short_name"`
+}
+
+func (r InlineQueryResultGame) getType() string {
+	return "game"
 }
 
 // InputMessageContent represents the content of a message to be sent as a result of an inline query
