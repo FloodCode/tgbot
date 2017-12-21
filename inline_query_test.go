c2c4cb20beb543fcb1ddfe7a99c8b3228e7fb507
FloodCode <flood.code@gmail.com>
2017-12-21 23:26:23 +0200

    Add all non-cached inline query results

--- inline_query_test.go
+++ inline_query_test.go
@@ -17,9 +17,18 @@ func assertEqual(t *testing.T, expected interface{}, actual interface{}) {
 func TestInlineQueryResult(t *testing.T) {
 	assertEqual(t, "article", InlineQueryResultArticle{}.getType())
 	assertEqual(t, "photo", InlineQueryResultPhoto{}.getType())
+	assertEqual(t, "gif", InlineQueryResultGif{}.getType())
+	assertEqual(t, "mpeg4_gif", InlineQueryResultMpeg4Gif{}.getType())
+	assertEqual(t, "video", InlineQueryResultVideo{}.getType())
+	assertEqual(t, "audio", InlineQueryResultAudio{}.getType())
+	assertEqual(t, "voice", InlineQueryResultVoice{}.getType())
+	assertEqual(t, "document", InlineQueryResultDocument{}.getType())
+	assertEqual(t, "location", InlineQueryResultLocation{}.getType())
+	assertEqual(t, "venue", InlineQueryResultVenue{}.getType())
+	assertEqual(t, "contact", InlineQueryResultContact{}.getType())
 }
 
-func TestDummyMethods(t *testing.T) {
+func TestDummyInputMessageContent(t *testing.T) {
 	InputTextMessageContent{}._dummyInputMessageContent()
 	InputLocationMessageContent{}._dummyInputMessageContent()
 	InputVenueMessageContent{}._dummyInputMessageContent()
