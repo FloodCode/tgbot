f4eb3ad15a4e15d1f363c12476583d03c625959d
FloodCode <flood.code@gmail.com>
2017-12-23 13:06:41 +0200

    Fix Game type

--- types.go
+++ types.go
@@ -131,7 +131,7 @@ type PhotoSize struct {
 type Game struct {
 	Title        string           `json:"title"`
 	Description  string           `json:"description"`
-	Photo        *PhotoSize       `json:"photo"`
+	Photo        []*PhotoSize     `json:"photo"`
 	Text         string           `json:"text"`
 	TextEntities []*MessageEntity `json:"text_entities"`
 	Animation    *Animation       `json:"animation"`
