4338dc0e463fc2bb9596f44b4b9fe64d2c4b0f80
FloodCode <flood.code@gmail.com>
2018-01-03 02:47:13 +0200

    Rearange telegram types as in API docs

--- types.go
+++ types.go
@@ -27,49 +27,6 @@ type WebhookInfo struct {
 	AllowedUpdates       []string `json:"allowed_updates"`
 }
 
-// Message represents a message
-type Message struct {
-	MessageID             int                `json:"message_id"`
-	From                  *User              `json:"from"`
-	Date                  int                `json:"date"`
-	Chat                  *Chat              `json:"chat"`
-	ForwardFrom           *User              `json:"forward_from"`
-	ForwardFromChat       *Chat              `json:"forward_from_chat"`
-	ForwardFromMessageID  int                `json:"forward_from_message_id"`
-	ForwardSignature      string             `json:"forward_signature"`
-	ForwardDate           int                `json:"forward_date"`
-	ReplyToMessage        *Message           `json:"reply_to_message"`
-	EditDate              int                `json:"edit_date"`
-	AuthorSignature       string             `json:"author_signature"`
-	Text                  string             `json:"text"`
-	Entities              []*MessageEntity   `json:"entities"`
-	Audio                 *Audio             `json:"audio"`
-	Document              *Document          `json:"document"`
-	Game                  *Game              `json:"game"`
-	Photo                 []*PhotoSize       `json:"photo"`
-	Sticker               *Sticker           `json:"sticker"`
-	Video                 *Video             `json:"video"`
-	Voice                 *Voice             `json:"voice"`
-	VideoNote             *VideoNote         `json:"video_note"`
-	NewChatMembers        []*User            `json:"new_chat_members"`
-	Caption               string             `json:"caption"`
-	Contact               *Contact           `json:"contact"`
-	Location              *Location          `json:"location"`
-	Venue                 *Venue             `json:"venue"`
-	NewChatMember         *User              `json:"new_chat_member"`
-	LeftChatMember        *User              `json:"left_chat_member"`
-	NewChatTitle          string             `json:"new_chat_title"`
-	NewChatPhoto          []*PhotoSize       `json:"new_chat_photo"`
-	DeleteChatPhoto       bool               `json:"delete_chat_photo"`
-	GroupChatCreated      bool               `json:"group_chat_created"`
-	SupergroupChatCreated bool               `json:"supergroup_chat_created"`
-	ChannelChatCreated    bool               `json:"channel_chat_created"`
-	MigrateToChatID       int64              `json:"migrate_to_chat_id"`
-	MigrateFromChatID     int64              `json:"migrate_from_chat_id"`
-	PinnedMessage         *Message           `json:"pinned_message"`
-	SuccessfulPayment     *SuccessfulPayment `json:"successful_payment"`
-}
-
 // User represents a user
 type User struct {
 	ID           int    `json:"id"`
@@ -93,12 +50,53 @@ type Chat struct {
 	Description                 string     `json:"description"`
 	InviteLink                  string     `json:"invite_link"`
 	PinnedMessage               *Message   `json:"pinned_message"`
+	StickerSetName              string     `json:"sticker_set_name"`
+	CanSetStickerSet            bool       `json:"can_set_sticker_set"`
 }
 
-// ChatPhoto represents a chat photo
-type ChatPhoto struct {
-	SmallFileID string `json:"small_file_id"`
-	BigFileID   string `json:"big_file_id"`
+// Message represents a message
+type Message struct {
+	MessageID             int                `json:"message_id"`
+	From                  *User              `json:"from"`
+	Date                  int                `json:"date"`
+	Chat                  *Chat              `json:"chat"`
+	ForwardFrom           *User              `json:"forward_from"`
+	ForwardFromChat       *Chat              `json:"forward_from_chat"`
+	ForwardFromMessageID  int                `json:"forward_from_message_id"`
+	ForwardSignature      string             `json:"forward_signature"`
+	ForwardDate           int                `json:"forward_date"`
+	ReplyToMessage        *Message           `json:"reply_to_message"`
+	EditDate              int                `json:"edit_date"`
+	MediaGroupID          string             `json:"media_group_id"`
+	AuthorSignature       string             `json:"author_signature"`
+	Text                  string             `json:"text"`
+	Entities              []*MessageEntity   `json:"entities"`
+	CaptionEntities       []*MessageEntity   `json:"caption_entities"`
+	Audio                 *Audio             `json:"audio"`
+	Document              *Document          `json:"document"`
+	Game                  *Game              `json:"game"`
+	Photo                 []*PhotoSize       `json:"photo"`
+	Sticker               *Sticker           `json:"sticker"`
+	Video                 *Video             `json:"video"`
+	Voice                 *Voice             `json:"voice"`
+	VideoNote             *VideoNote         `json:"video_note"`
+	Caption               string             `json:"caption"`
+	Contact               *Contact           `json:"contact"`
+	Location              *Location          `json:"location"`
+	Venue                 *Venue             `json:"venue"`
+	NewChatMembers        []*User            `json:"new_chat_members"`
+	LeftChatMember        *User              `json:"left_chat_member"`
+	NewChatTitle          string             `json:"new_chat_title"`
+	NewChatPhoto          []*PhotoSize       `json:"new_chat_photo"`
+	DeleteChatPhoto       bool               `json:"delete_chat_photo"`
+	GroupChatCreated      bool               `json:"group_chat_created"`
+	SupergroupChatCreated bool               `json:"supergroup_chat_created"`
+	ChannelChatCreated    bool               `json:"channel_chat_created"`
+	MigrateToChatID       int64              `json:"migrate_to_chat_id"`
+	MigrateFromChatID     int64              `json:"migrate_from_chat_id"`
+	PinnedMessage         *Message           `json:"pinned_message"`
+	Invoice               *Invoice           `json:"invoice"`
+	SuccessfulPayment     *SuccessfulPayment `json:"successful_payment"`
 }
 
 // MessageEntity represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
@@ -108,6 +106,14 @@ type MessageEntity struct {
 	Length int    `json:"length"`
 	URL    string `json:"url"`
 	User   *User  `json:"user"`
+}
+
+// PhotoSize represents one size of a photo or a file / sticker thumbnail
+type PhotoSize struct {
+	FileID   string `json:"file_id"`
+	Width    int    `json:"width"`
+	Height   int    `json:"height"`
+	FileSize int    `json:"file_size"`
 }
 
 // Audio represents an audio file to be treated as music by the Telegram clients
@@ -127,63 +133,6 @@ type Document struct {
 	FileName string     `json:"file_name"`
 	MimeType string     `json:"mime_type"`
 	FileSize int        `json:"file_size"`
-}
-
-// PhotoSize represents one size of a photo or a file / sticker thumbnail
-type PhotoSize struct {
-	FileID   string `json:"file_id"`
-	Width    int    `json:"width"`
-	Height   int    `json:"height"`
-	FileSize int    `json:"file_size"`
-}
-
-// Game represents a game.
-// Use BotFather to create and edit games, their short names will act as unique identifiers.
-type Game struct {
-	Title        string           `json:"title"`
-	Description  string           `json:"description"`
-	Photo        []*PhotoSize     `json:"photo"`
-	Text         string           `json:"text"`
-	TextEntities []*MessageEntity `json:"text_entities"`
-	Animation    *Animation       `json:"animation"`
-}
-
-// Animation represents an animation file to be displayed in the message containing a game
-type Animation struct {
-	FileID   string     `json:"file_id"`
-	Thumb    *PhotoSize `json:"thumb"`
-	FileName string     `json:"file_name"`
-	MimeType string     `json:"mime_type"`
-	FileSize int        `json:"file_size"`
-}
-
-// Sticker represents a sticker
-type Sticker struct {
-	FileID       string        `json:"file_id"`
-	Width        int           `json:"width"`
-	Height       int           `json:"height"`
-	Thumb        *PhotoSize    `json:"thumb"`
-	Emoji        string        `json:"emoji"`
-	SetName      string        `json:"set_name"`
-	MaskPosition *MaskPosition `json:"mask_position"`
-	FileSize     int           `json:"file_size"`
-}
-
-// StickerSet represents a sticker set
-type StickerSet struct {
-	Name          string     `json:"name"`
-	Title         string     `json:"title"`
-	ContainsMasks bool       `json:"contains_masks"`
-	Stickers      []*Sticker `json:"stickers"`
-}
-
-// MaskPosition describes the position on faces where a mask should be placed by default.
-// TODO: Turn it into a method.
-type MaskPosition struct {
-	Point  string  `json:"point"`
-	XShift float32 `json:"x_shift"`
-	YShift float32 `json:"y_shift"`
-	Scale  float32 `json:"scale"`
 }
 
 // Video represents a video file
@@ -236,6 +185,126 @@ type Venue struct {
 	FoursquareID string    `json:"foursquare_id"`
 }
 
+// UserProfilePhotos represents a user's profile pictures
+type UserProfilePhotos struct {
+	TotalCount int            `json:"total_count"`
+	Photos     [][]*PhotoSize `json:"photos"`
+}
+
+// File represents a file ready to be downloaded
+type File struct {
+	FileID   string `json:"file_id"`
+	FileSize int    `json:"file_size"`
+	FilePath string `json:"file_path"`
+}
+
+// KeyboardButton represents one button of the reply keyboard
+type KeyboardButton struct {
+	Text            string `json:"text"`
+	RequestContact  bool   `json:"request_contact"`
+	RequestLocation bool   `json:"request_location"`
+}
+
+// InlineKeyboardButton represents one button of an inline keyboard.
+// You must use exactly one of the optional fields.
+type InlineKeyboardButton struct {
+	Text                         string        `json:"text"`
+	URL                          string        `json:"url"`
+	CallbackData                 string        `json:"callback_data"`
+	SwitchInlineQuery            string        `json:"switch_inline_query"`
+	SwitchInlineQueryCurrentChat string        `json:"switch_inline_query_current_chat"`
+	CallbackGame                 *CallbackGame `json:"callback_game"`
+	Pay                          bool          `json:"pay"`
+}
+
+// CallbackQuery represents an incoming callback query from a callback button
+// in an inline keyboard. If the button that originated the query was attached
+// to a message sent by the bot, the field message will be present.
+type CallbackQuery struct {
+	ID              string   `json:"id"`
+	From            *User    `json:"from"`
+	Message         *Message `json:"message"`
+	InlineMessageID string   `json:"inline_message_id"`
+	ChatInstance    string   `json:"chat_instance"`
+	Data            string   `json:"data"`
+	GameShortName   string   `json:"game_short_name"`
+}
+
+// ChatPhoto represents a chat photo
+type ChatPhoto struct {
+	SmallFileID string `json:"small_file_id"`
+	BigFileID   string `json:"big_file_id"`
+}
+
+// ChatMember represents information about one member of a chat
+type ChatMember struct {
+	User                  *User  `json:"user"`
+	Status                string `json:"status"`
+	UntilDate             int    `json:"until_date"`
+	CanBeEdited           bool   `json:"can_be_edited"`
+	CanChangeInfo         bool   `json:"can_change_info"`
+	CanPostMessages       bool   `json:"can_post_messages"`
+	CanEditMessages       bool   `json:"can_edit_messages"`
+	CanDeleteMessages     bool   `json:"can_delete_messages"`
+	CanInviteUsers        bool   `json:"can_invite_users"`
+	CanRestrictMembers    bool   `json:"can_restrict_members"`
+	CanPinMessages        bool   `json:"can_pin_messages"`
+	CanPromoteMembers     bool   `json:"can_promote_members"`
+	CanSendMessages       bool   `json:"can_send_messages"`
+	CanSendMediaMessages  bool   `json:"can_send_media_messages"`
+	CanSendOtherMessages  bool   `json:"can_send_other_messages"`
+	CanAddWebPagePreviews bool   `json:"can_add_web_page_previews"`
+}
+
+// Sticker represents a sticker
+type Sticker struct {
+	FileID       string        `json:"file_id"`
+	Width        int           `json:"width"`
+	Height       int           `json:"height"`
+	Thumb        *PhotoSize    `json:"thumb"`
+	Emoji        string        `json:"emoji"`
+	SetName      string        `json:"set_name"`
+	MaskPosition *MaskPosition `json:"mask_position"`
+	FileSize     int           `json:"file_size"`
+}
+
+// StickerSet represents a sticker set
+type StickerSet struct {
+	Name          string     `json:"name"`
+	Title         string     `json:"title"`
+	ContainsMasks bool       `json:"contains_masks"`
+	Stickers      []*Sticker `json:"stickers"`
+}
+
+// MaskPosition describes the position on faces where a mask should be placed by default.
+// TODO: Turn it into a method.
+type MaskPosition struct {
+	Point  string  `json:"point"`
+	XShift float32 `json:"x_shift"`
+	YShift float32 `json:"y_shift"`
+	Scale  float32 `json:"scale"`
+}
+
+// Game represents a game.
+// Use BotFather to create and edit games, their short names will act as unique identifiers.
+type Game struct {
+	Title        string           `json:"title"`
+	Description  string           `json:"description"`
+	Photo        []*PhotoSize     `json:"photo"`
+	Text         string           `json:"text"`
+	TextEntities []*MessageEntity `json:"text_entities"`
+	Animation    *Animation       `json:"animation"`
+}
+
+// Animation represents an animation file to be displayed in the message containing a game
+type Animation struct {
+	FileID   string     `json:"file_id"`
+	Thumb    *PhotoSize `json:"thumb"`
+	FileName string     `json:"file_name"`
+	MimeType string     `json:"mime_type"`
+	FileSize int        `json:"file_size"`
+}
+
 // Invoice contains basic information about a invoice
 type Invoice struct {
 	Title          string `json:"title"`
@@ -274,61 +343,9 @@ type ShippingAddress struct {
 	PostCode    string `json:"post_code"`
 }
 
-// UserProfilePhotos represents a user's profile pictures
-type UserProfilePhotos struct {
-	TotalCount int            `json:"total_count"`
-	Photos     [][]*PhotoSize `json:"photos"`
-}
-
-// File represents a file ready to be downloaded
-type File struct {
-	FileID   string `json:"file_id"`
-	FileSize int    `json:"file_size"`
-	FilePath string `json:"file_path"`
-}
-
-// ChatMember represents information about one member of a chat
-type ChatMember struct {
-	User                  *User  `json:"user"`
-	Status                string `json:"status"`
-	UntilDate             int    `json:"until_date"`
-	CanBeEdited           bool   `json:"can_be_edited"`
-	CanChangeInfo         bool   `json:"can_change_info"`
-	CanPostMessages       bool   `json:"can_post_messages"`
-	CanEditMessages       bool   `json:"can_edit_messages"`
-	CanDeleteMessages     bool   `json:"can_delete_messages"`
-	CanInviteUsers        bool   `json:"can_invite_users"`
-	CanRestrictMembers    bool   `json:"can_restrict_members"`
-	CanPinMessages        bool   `json:"can_pin_messages"`
-	CanPromoteMembers     bool   `json:"can_promote_members"`
-	CanSendMessages       bool   `json:"can_send_messages"`
-	CanSendMediaMessages  bool   `json:"can_send_media_messages"`
-	CanSendOtherMessages  bool   `json:"can_send_other_messages"`
-	CanAddWebPagePreviews bool   `json:"can_add_web_page_previews"`
-}
-
 // CallbackGame is a placeholder, currently holds no information.
 // Use BotFather to set up your game.
 type CallbackGame struct{}
-
-// InlineKeyboardButton represents one button of an inline keyboard.
-// You must use exactly one of the optional fields.
-type InlineKeyboardButton struct {
-	Text                         string        `json:"text"`
-	URL                          string        `json:"url"`
-	CallbackData                 string        `json:"callback_data"`
-	SwitchInlineQuery            string        `json:"switch_inline_query"`
-	SwitchInlineQueryCurrentChat string        `json:"switch_inline_query_current_chat"`
-	CallbackGame                 *CallbackGame `json:"callback_game"`
-	Pay                          bool          `json:"pay"`
-}
-
-// KeyboardButton represents one button of the reply keyboard
-type KeyboardButton struct {
-	Text            string `json:"text"`
-	RequestContact  bool   `json:"request_contact"`
-	RequestLocation bool   `json:"request_location"`
-}
 
 // InlineQuery represents an incoming inline query.
 // When the user sends an empty query, your bot could return some default or trending results.
@@ -348,19 +365,6 @@ type ChosenInlineResult struct {
 	Location        *Location `json:"location"`
 	InlineMessageID string    `json:"inline_message_id"`
 	Query           string    `json:"query"`
-}
-
-// CallbackQuery represents an incoming callback query from a callback button
-// in an inline keyboard. If the button that originated the query was attached
-// to a message sent by the bot, the field message will be present.
-type CallbackQuery struct {
-	ID              string   `json:"id"`
-	From            *User    `json:"from"`
-	Message         *Message `json:"message"`
-	InlineMessageID string   `json:"inline_message_id"`
-	ChatInstance    string   `json:"chat_instance"`
-	Data            string   `json:"data"`
-	GameShortName   string   `json:"game_short_name"`
 }
 
 // ShippingQuery represents a shipping address
